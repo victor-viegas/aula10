ba7e79026029d6ef7ebe96a7ecf126939f948fbe
Victor Rafaeel <rafaeel410@gmail.com>
2024-03-07 19:45:28 -0300

    Javascript do menu e alterações do design

--- js/home.js
+++ js/home.js
@@ -1,9 +1,11 @@
-const menuButton = document.getElementById("menu-button");
+const menuButton1 = document.getElementById('menu-button-1');
+const menuButton2 = document.getElementById('menu-button-2');
+
+
 function toggleMenu(event) {
-    const menu = document.getElementsByClassName("container-nav");
-    menu.idList.toggle('active');
-    const active = menu.idList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
+    const menu = document.getElementById('container-nav');
+    menu.classList.toggle('active');
 }
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton1.addEventListener('click', toggleMenu);
+menuButton2.addEventListener('click', toggleMenu);
