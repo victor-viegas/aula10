87a632f44b7e82f92d85fde8d67be8523a4cdf91
Victor Rafaeel <rafaeel410@gmail.com>
2024-03-12 11:36:08 -0300

    importação do glider e apllicação para criação de um carrosel

--- js/home.js
+++ js/home.js
@@ -9,3 +9,53 @@ function toggleMenu(event) {
 
 menuButton1.addEventListener('click', toggleMenu);
 menuButton2.addEventListener('click', toggleMenu);
+
+const $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+new Glider($simpleCarousel, {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    duration: 0.25,
+    arrows: {
+        prev: ".js-carousel--simple-prev",
+        next: ".js-carousel--simple-next",
+    },
+    responsive: [
+        {
+            breakpoint: 300,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
